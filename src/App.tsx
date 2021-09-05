941db9361ab5cd15d84220fa390fc6be10211b9c
kaspars <kaspars.putnins19@gmail.com>
2021-09-05 18:46:25 +0300

    feat: added webpack env variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import image from "./assets/magic.gif";
 const App: FunctionComponent = () => {
   return (
     <div>
-      <h1>Welcome to React</h1>
+      <h1>Welcome to React - {process.env.name}</h1>
       <img src={image} alt="React logo" />
     </div>
   );
