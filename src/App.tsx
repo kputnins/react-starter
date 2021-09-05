9d67f0647ff0c45543050a616d0492e6cc25798c
kaspars <kaspars.putnins19@gmail.com>
2021-09-05 18:17:59 +0300

    feat: added style and image loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { FunctionComponent } from "react";
 
+import image from "./assets/magic.gif";
+
 const App: FunctionComponent = () => {
   return (
     <div>
       <h1>test</h1>
+      <img src={image} alt="React logo" />
     </div>
   );
 };
