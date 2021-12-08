986ee4b2e24370ad68ffb1fd75f19fdc400dfd96
Kaspars <kaspars.putnins@if.lv>
2021-12-08 17:54:47 +0200

    chore: added chrome vs code debug config

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -5,11 +5,10 @@ import classes from './Counter.module.scss';
 const Counter: FunctionComponent = () => {
   const [count, setCount] = useState(0);
 
+  const handleCounterClick = () => setCount((currentCount) => currentCount + 1);
+
   return (
-    <button
-      className={classes.Counter}
-      onClick={() => setCount((currentCount) => currentCount + 1)}
-    >
+    <button className={classes.Counter} onClick={handleCounterClick}>
       Count {count}
     </button>
   );
